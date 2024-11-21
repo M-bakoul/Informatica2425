82614f9e652ad3fadb12d898d496f59f75511a5b
M-bakoul <mohamed.bakloul@galileiostiglia.edu.it>
2024-11-21 08:57:23 +0100

    Update 21112024.c

--- 21112024.c
+++ 21112024.c
@@ -4,10 +4,13 @@ int main(){
     int num;
     int somma;
     
-    somma=(1000/10) % 10+
-          (100/10) % 10+
-          (10/10) % 10+
-           10 % 10;
+    printf("Inserisci un numero");
+    scanf("%d", &num);
+    
+    somma=(num / 1000) % 10+
+          (num / 100) % 10+
+          (num / 10) % 10+
+           num % 10;
         
     if(somma%3 == 0){
         printf("Il numero e divisibile per 3");
